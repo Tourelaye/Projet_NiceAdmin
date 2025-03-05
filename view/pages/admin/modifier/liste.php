3bd04abf96e7a2535255ed7ea591c019ba2dd1da
Abdoulaye Toure <ablayedjiby81@gmail.com>
2025-03-05 04:47:55 +0000

    Mise a jour modification

---
+++ view/pages/admin/modifier/liste.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- ==================== Section Header ==================== -->
+<?php require_once("../../../sections/admin/header.php")?>
+
+<body>
+
+  <!-- ==================== Section Menu Haut ==================== -->
+  <?php require_once("../../../sections/admin/menuHaut.php")?>  
+
+  <!-- ==================== Section Menu gauche ==================== -->
+  <?php require_once("../../../sections/admin/menuGauche.php")?>  
+
+  <!-- ==================== Section Contenu ==================== -->
+   
+
+  <!-- ==================== Section Footer ==================== -->
+  <?php require_once("../../../sections/admin/footer.php")?>  
+
+  <!-- ==================== Section Base JS ==================== -->
+  <?php require_once("../../../sections/admin/script.php")?>
+
+</body>
+
+</html>
