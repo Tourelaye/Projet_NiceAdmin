4110ab53aa0b665f9396c49b1d07538b602a80c9
Abdoulaye Toure <ablayedjiby81@gmail.com>
2025-03-12 00:58:35 +0000

    New enregistrement

--- view/sections/admin/menuHaut.php
+++ view/sections/admin/menuHaut.php
@@ -3,7 +3,7 @@
 <div class="d-flex align-items-center justify-content-between">
   <a href="index.html" class="logo d-flex align-items-center">
     <img src="public/templates/templateAdmin/assets/img/logo.png" alt="">
-    <span class="d-none d-lg-block">NiceAdmin</span>
+    <span class="d-none d-lg-block">MoLa Technologie</span>
   </a>
   <i class="bi bi-list toggle-sidebar-btn"></i>
 </div><!-- End Logo -->
@@ -168,13 +168,13 @@
     <li class="nav-item dropdown pe-3">
 
       <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-        <img src="public/templates/templateAdmin/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-        <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+        <img src="public/templates/templateAdmin/assets/img/laye.jpg" alt="Profile" class="rounded-circle">
+        <span class="d-none d-md-block dropdown-toggle ps-2">Abdoulaye Toure</span>
       </a><!-- End Profile Iamge Icon -->
 
       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
         <li class="dropdown-header">
-          <h6>Kevin Anderson</h6>
+          <h6>Abdoulaye Toure</h6>
           <span>Web Designer</span>
         </li>
         <li>
@@ -182,9 +182,9 @@
         </li>
 
         <li>
-          <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+          <a class="dropdown-item d-flex align-items-center" href="pages-etudiant.html">
             <i class="bi bi-person"></i>
-            <span>My Profile</span>
+            <span>Ajouter</span>
           </a>
         </li>
         <li>
@@ -192,9 +192,9 @@
         </li>
 
         <li>
-          <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+          <a class="dropdown-item d-flex align-items-center" href="pages-note.html">
             <i class="bi bi-gear"></i>
-            <span>Account Settings</span>
+            <span>Note</span>
           </a>
         </li>
         <li>
@@ -202,9 +202,9 @@
         </li>
 
         <li>
-          <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
+          <a class="dropdown-item d-flex align-items-center" href="pages-evaluation.html">
             <i class="bi bi-question-circle"></i>
-            <span>Need Help?</span>
+            <span>Evaluation</span>
           </a>
         </li>
         <li>
@@ -212,7 +212,7 @@
         </li>
 
         <li>
-          <a class="dropdown-item d-flex align-items-center" href="#">
+          <a class="dropdown-item d-flex align-items-center" href="pages-deconnexion.html">
             <i class="bi bi-box-arrow-right"></i>
             <span>Sign Out</span>
           </a>
